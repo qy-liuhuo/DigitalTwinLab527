d1a21d0da47f8993fc049b48637c90c79ede7bb4
mahaotian's debian <maht2001@163.com>
2024-12-11 21:52:30 +0800

    修正坐标转换

--- BackEnd/DTLab/dtlab-common/src/main/java/org/mobinets/dtlab/common/utils/CoordinateConverter.java
+++ BackEnd/DTLab/dtlab-common/src/main/java/org/mobinets/dtlab/common/utils/CoordinateConverter.java
@@ -6,7 +6,7 @@ public class CoordinateConverter {
     private static final double camera_x = 5640;
     private static final double camera_y = 1050;
     private static final double camera_z = 2150;
-    private static final double camera_angle = 30;
+    private static final double camera_angle = 30.0;
 
     private static final double room_x = 12230;
 
@@ -17,8 +17,8 @@ public class CoordinateConverter {
     public static Coordinates convert(Coordinates coordinates) {
         Coordinates newCoordinates = new Coordinates();
         newCoordinates.setZ(0);
-        newCoordinates.setX(camera_x + coordinates.getX());
-        newCoordinates.setY(coordinates.getZ()/Math.cos(camera_angle) + camera_y);
+        newCoordinates.setX((camera_x + coordinates.getX() - room_x / 2) / 1000);
+        newCoordinates.setY((coordinates.getZ() * Math.sin(Math.toRadians(camera_angle)) + camera_y - room_y / 2) / 1000);
         return newCoordinates;
     }
 }
